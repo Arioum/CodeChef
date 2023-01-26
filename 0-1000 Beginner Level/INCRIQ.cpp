c48d0109d7bd6297d4193a97a89d936b8b4f894c
arioum <amoghrao121@gmail.com>
2023-01-26 12:34:23 +0530

    added INCRIQ solution

---
+++ 0-1000 Beginner Level/INCRIQ.cpp
@@ -0,0 +1,18 @@
+// Problem link -
+// https://www.codechef.com/problems/INCRIQ
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	cin>>x;
+	
+	if(x+7>170)
+	{
+	    cout<<"YES\n";
+	} else {
+	    cout<<"NO\n";
+	}
+	return 0;
+}
