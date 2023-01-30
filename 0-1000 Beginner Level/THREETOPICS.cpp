f3efc5a4a4b9bb288999e911ef1dfe03e54d9c9c
arioum <amoghrao121@gmail.com>
2023-01-30 20:50:55 +0530

    added THREETOPICS

---
+++ 0-1000 Beginner Level/THREETOPICS.cpp
@@ -0,0 +1,18 @@
+// Problem link -
+// https://www.codechef.com/problems/THREETOPICS
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,c,x;
+	cin>>a>>b>>c>>x;
+	
+	if(a==x || b==x || c==x)
+	{
+	    cout<<"Yes\n";
+	} else {
+	    cout<<"No\n";
+	}
+	return 0;
+}
