4b5f66e10ed57d6148128d0f8b4f794e5d77387f
arioum <amoghrao121@gmail.com>
2023-01-30 20:55:00 +0530

    added CREDSCORE

---
+++ 0-1000 Beginner Level/CREDSCORE.cpp
@@ -0,0 +1,17 @@
+// Problem link -
+// https://www.codechef.com/problems/CREDSCORE
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	cin>>x;
+	
+	if(x>=750){
+	    cout<<"Yes\n";
+	} else {
+	    cout<<"No\n";
+	}
+	return 0;
+}
