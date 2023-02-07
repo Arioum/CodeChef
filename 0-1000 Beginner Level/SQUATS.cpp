4f7184d279d248c43668d8c1268f63fa94e41164
arioum <amoghrao121@gmail.com>
2023-02-07 23:38:16 +0530

    added SQUATS

---
+++ 0-1000 Beginner Level/SQUATS.cpp
@@ -0,0 +1,19 @@
+// Problem link -
+// https://www.codechef.com/problems/SQUATS
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	   int x;
+	   cin>>x;
+	   
+	   cout<<x*15<<"\n";
+	}
+	return 0;
+}
