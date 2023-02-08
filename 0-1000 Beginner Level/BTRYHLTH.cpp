fabeccdd7c3da2efc7ddef972a781e0e62fd8eae
arioum <amoghrao121@gmail.com>
2023-02-08 23:25:01 +0530

    BTRYHLTH added

---
+++ 0-1000 Beginner Level/BTRYHLTH.cpp
@@ -0,0 +1,24 @@
+// Problem link -
+// https://www.codechef.com/problems/BTRYHLTH
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	   int x;
+	   cin>>x;
+	   
+	   if(x>=80)
+	   {
+	       cout<<"YES\n";
+	   } else {
+	       cout<<"NO\n";
+	   }
+	}
+	return 0;
+}
