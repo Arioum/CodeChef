3e9972f4fe33799c25b0c81485fecece50b039f4
arioum <amoghrao121@gmail.com>
2023-02-17 23:50:30 +0530

    added ADVANCE

---
+++ 0-1000 Beginner Level/ADVANCE.cpp
@@ -0,0 +1,24 @@
+// Problem link -
+// https://www.codechef.com/problems/ADVANCE
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    
+	    if(y>=x && y<=x+200)
+	    {
+	        cout<<"YES\n";
+	    } else {
+	        cout<<"NO\n";
+	    }
+	}
+	return 0;
+}
