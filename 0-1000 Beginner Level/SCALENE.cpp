8f362bc081c33f2233ea5ea5e32d62048af08037
arioum <amoghrao121@gmail.com>
2023-02-17 23:54:56 +0530

    added SCALENE

---
+++ 0-1000 Beginner Level/SCALENE.cpp
@@ -0,0 +1,23 @@
+// Problem link -
+// https://www.codechef.com/problems/SCALENE
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    
+	    if(a!=b && b!=c && c!=a) {
+	        cout<<"YES\n";
+	    } else {
+	        cout<<"NO\n";
+	    }
+	}
+	return 0;
+}
